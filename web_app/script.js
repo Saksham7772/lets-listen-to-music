8d1cd585a9d4e0c4038cf0513e9610e42b14788e
SPUXEL <spuxel@yandex.ru>
2023-02-27 22:07:22 +0000

    WebApp: Verification of completed data

--- web_app/script.js
+++ web_app/script.js
@@ -4,7 +4,21 @@ const form = {
     button_title: document.getElementById('ad__button_title'),
     button_url: document.getElementById('ad__button_url'),
     view_limit: document.getElementById('ad__view_limit'),
+
     button: document.querySelector('.button'),
+}
+
+function checkForm() {
+    const text = form.text.getElementsByTagName('input')[0].value
+    const button_title = form.button_title.getElementsByTagName('input')[0].value
+    const button_url = form.button_url.getElementsByTagName('input')[0].value
+    const view_limit = form.view_limit.getElementsByTagName('input')[0].value
+
+    if (text && button_title && button_url && view_limit) {
+        form.button.classList.remove('disable')
+    } else {
+        form.button.classList.add('disable')
+    }
 }
 
 function handleInput(e, name) {
@@ -14,14 +28,12 @@ function handleInput(e, name) {
     } else {
         form[name].classList.remove('filed')
     }
-}
-
-function checkForm() {
-    form.text.getElementsByTagName('input')
+    checkForm()
 }
 
 form.text.oninput = (e) => handleInput(e, 'text')
 form.button_title.oninput = (e) => handleInput(e, 'button_title')
 form.button_url.oninput = (e) => handleInput(e, 'button_url')
 form.view_limit.oninput = (e) => handleInput(e, 'view_limit')
-form.button.onclick = checkForm
+
+form.button.onclick = () => alert('Done')
