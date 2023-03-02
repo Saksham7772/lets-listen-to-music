5f10830d4c0556c70aef0e1c4d4ee0e978d3477e
SPUXEL <spuxel@yandex.ru>
2023-03-02 07:27:55 +0000

    Update WebApp

--- web_app/script.js
+++ web_app/script.js
@@ -1,33 +1,28 @@
-"use strict"
 
-document.addEventListener('DOMContentLoaded', function(){
+// TELEGRAM
+let tg = window.Telegram.WebApp;
+//tg.expand();
 
-    // TELEGRAM
-    let tg = window.Telegram.WebApp;
-    //tg.expand();
-
-    tg.MainButton.text = 'Отправить'
-    //tg.MainButton.color = '#'
+tg.MainButton.text = 'Отправить'
+//tg.MainButton.color = '#'
 
 
-    // FORM
-    let formData = {};
-    const form = document.querySelector('form');
+// FORM
+let formData = {};
+const form = document.querySelector('form');
 
-    form.addEventListener('input', function(event){
-        formData[event.target.name] = event.target.value;
-    });
+form.addEventListener('input', function(event){
+    formData[event.target.name] = event.target.value;
+});
 
-    form.addEventListener('submit', async function(event){
-        event.preventDefault();
-        form.classList.add('_sending');
+form.addEventListener('submit', async function(event){
+    event.preventDefault();
+    form.classList.add('_sending');
 
-        tg.MainButton.show();
-    });
+    tg.MainButton.show();
+});
 
 
-    // SEND DATA TO TELEGRAM
-    Telegram.WebApp.onEvent('mainButtonClicked', function(){
-        tg.sendData(formData);
-    });
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+    tg.sendData(JSON.stringify(formData);
 });
